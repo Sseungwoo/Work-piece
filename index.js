7a0c7322d3d8e537353c9bd28a9d018d3e94dff7
Sseungwoo <58283195+Sseungwoo@users.noreply.github.com>
2020-01-08 14:44:41 +0900

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-$("#main1").mouseover(function(){
-    $(this).children(".submenu").stop().slideDown();
-});
-$("#main1").mouseleave(function(){
-    $(this).children(".submenu").stop().slideUp();
-});
